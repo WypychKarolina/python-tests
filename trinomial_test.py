6596343fb89ce776d2273ea42f8505445c37ccf0
Karolina Wypych <wypych.kar.0101@gmail.com>
2023-02-13 11:47:20 +0100

    add testy

--- trinomial_test.py
+++ trinomial_test.py
@@ -1,15 +1,29 @@
-
 import unittest
 from trinomial import *
 
 class Trinomial_function_test(unittest.TestCase):
 
-    # TODO enalbe this test:
-    #def test_all_zeros(self):
-    #    assert trinomial(0, 0, 0) == None
+    def test_all_zeros(self):
+        assert trinomial(0, 0, 0) == None
 
     def test_all_ones(self):
         assert trinomial(1, 1, 1) == ()
+
+    def test_invalid_argument(self):
+        assert trinomial("a", 2, 8) == None
+        assert trinomial(25,77,[1,2]) == None
+
+    def test_no_3_args(self):
+        assert trinomial([1,2]) == None
+        assert trinomial(1,2,4,7.0) == None
+
+    def test_check_invalid_argumant(self):
+        assert check1("aa") == (False, None)
     
+    def test_check_correct_case(self):
+        assert check1(1,5,3)[0] == True
+        assert type(check1(1,5,3)[1]) == tuple
+
+
 if __name__ == '__main__':
     unittest.main()
